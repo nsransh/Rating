ce45aedc3c1fff8a8f4f7af494587f9ba69cc328
nsransh <nurani.solechah23@gmail.com>
2023-05-19 13:18:19 +0700

    update structure logic with function, and rate with paw

--- app/src/main/java/com/example/ratingapplication/ui/RatingFragment.kt
+++ app/src/main/java/com/example/ratingapplication/ui/RatingFragment.kt
@@ -1,6 +1,7 @@
 package com.example.ratingapplication.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -56,40 +57,76 @@ class RatingFragment : Fragment() {
                 clickStar(star)
             }
 
+            swTips.setOnClickListener {
+                swTips.setTextColor(resources.getColor(R.color.pinkpaw))
+            }
+
             btnSubmit.setOnClickListener {
                 if (swTips.isChecked){
-                    Toast.makeText(requireContext(),"$star Star with Tips",Toast.LENGTH_LONG).show()
+                    Toast.makeText(requireContext(),"$star Rate with Tips",Toast.LENGTH_LONG).show()
                 }else{
-                    Toast.makeText(requireContext(),"$star Star and no Tips",Toast.LENGTH_LONG).show()
+                    Toast.makeText(requireContext(),"$star Rate and No Tips",Toast.LENGTH_LONG).show()
                 }
+                resetRating()
+                reset()
             }
         }
     }
 
     fun clickStar(jmlh:Int){
         binding.apply {
-            for (i in 1..5){
-                when (i){
-                    1 -> iv1.setImageResource(R.drawable.baseline_star_border_24_yellow)
-                    2 -> iv2.setImageResource(R.drawable.baseline_star_border_24_yellow)
-                    3 -> iv3.setImageResource(R.drawable.baseline_star_border_24_yellow)
-                    4 -> iv4.setImageResource(R.drawable.baseline_star_border_24_yellow)
-                    5 -> iv5.setImageResource(R.drawable.baseline_star_border_24_yellow)
-                }
-            }
+            tvRating.setTextColor(resources.getColor(R.color.pinkpaw))
+            resetRating()
             for (i in 1..jmlh){
                 when (i){
-                    1 -> iv1.setImageResource(R.drawable.baseline_star_24_yellow)
-                    2 -> iv2.setImageResource(R.drawable.baseline_star_24_yellow)
-                    3 -> iv3.setImageResource(R.drawable.baseline_star_24_yellow)
-                    4 -> iv4.setImageResource(R.drawable.baseline_star_24_yellow)
-                    5 -> iv5.setImageResource(R.drawable.baseline_star_24_yellow)
+                    1 -> iv1.setImageResource(R.drawable.pawprint)
+                    2 -> iv2.setImageResource(R.drawable.pawprint)
+                    3 -> iv3.setImageResource(R.drawable.pawprint)
+                    4 -> iv4.setImageResource(R.drawable.pawprint)
+                    5 -> iv5.setImageResource(R.drawable.pawprint)
                 }
             }
-
+            if (jmlh<5){
+                Log.e("jmlh", "$jmlh")
+                for (i in jmlh+1..5){
+                    when(i){
+                        1 -> iv1.setColorFilter(resources.getColor(R.color.pinkpaw))
+                        2 -> iv2.setColorFilter(resources.getColor(R.color.pinkpaw))
+                        3 -> iv3.setColorFilter(resources.getColor(R.color.pinkpaw))
+                        4 -> iv4.setColorFilter(resources.getColor(R.color.pinkpaw))
+                        5 -> iv5.setColorFilter(resources.getColor(R.color.pinkpaw))
+                    }
+                }
+            }
             btnSubmit.isVisible = true
         }
 
+    }
+
+    fun resetRating(){
+        binding.apply {
+            iv1.setImageResource(R.drawable.pawprint1)
+            iv2.setImageResource(R.drawable.pawprint1)
+            iv3.setImageResource(R.drawable.pawprint1)
+            iv4.setImageResource(R.drawable.pawprint1)
+            iv5.setImageResource(R.drawable.pawprint1)
+            iv1.setColorFilter(null)
+            iv2.setColorFilter(null)
+            iv3.setColorFilter(null)
+            iv4.setColorFilter(null)
+            iv5.setColorFilter(null)
+        }
+
+
+    }
+
+    fun reset(){
+        binding.apply {
+            tvRating.setTextColor(resources.getColor(R.color.black))
+            swTips.isChecked = false
+            swTips.setTextColor(resources.getColor(R.color.black))
+            btnSubmit.isVisible = false
+        }
     }
 
     companion object {
